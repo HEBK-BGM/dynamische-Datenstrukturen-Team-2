4abfb25879e8e35219deebac84fc4786abf289b6
TimoDragon <94261729+TimoDragon@users.noreply.github.com>
2022-11-18 17:41:13 +0100

    [Add] Sound/Video Types

--- src/main/java/de/hebk/media/sound/SoundType.java
+++ src/main/java/de/hebk/media/sound/SoundType.java
@@ -1,4 +1,13 @@
 package de.hebk.media.sound;
 
 public enum SoundType {
+    QUESTION,
+    WRONG_ANSWER,
+    RIGHT_ANSWER,
+    IDLE,
+    START,
+    WIN,
+    HALF_JOKER,
+    TELEPHONE_JOKER,
+    AUDIENCE_JOKER
 }
