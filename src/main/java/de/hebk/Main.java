e4ca0f20654899f03ba47122af1eecfad62e15e7
TimoDragon <timo.kruse@hebk.de>
2022-12-18 19:00:22 +0100

    Changes by Jonas

--- src/main/java/de/hebk/Main.java
+++ src/main/java/de/hebk/Main.java
@@ -1,6 +1,11 @@
 package de.hebk;
 
+import de.hebk.gamemodes.Normal;
 import de.hebk.gui.StartGui;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
